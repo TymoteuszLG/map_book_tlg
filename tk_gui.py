831db3404f1736e9638eafb5097f45064ceeed4a
TymoteuszLG <167193601+TymoteuszLG@users.noreply.github.com>
2024-05-28 19:21:27 +0200

    wip: everything

--- tk_gui.py
+++ tk_gui.py
@@ -5,69 +5,78 @@ def prosta_funkcja():
 
 root=Tk()
 root.geometry('800x700')
-root.title('Map Book.Sg')
+root.title('Map Book Sg')
 
-#ramki do porządkowania struktury
+# ramki do porządkowania struktury
 ramka_lista_uzytkownikow=Frame(root)
 ramka_formularz=Frame(root)
 ramka_pokaz_szczegoly=Frame(root)
-ramka_mapa=Frame(root)
+# ramka_mapa=Frame(root)
 
-ramka_lista_uzytkownikow.grid(row=0, column=0)
+ramka_lista_uzytkownikow.grid(row=0, column=0,padx=50)
 ramka_formularz.grid(row=0, column=1)
-ramka_pokaz_szczegoly.grid(row=1, column=0)
-ramka_mapa.grid(row=2, column=0)
+ramka_pokaz_szczegoly.grid(row=1, column=0,columnspan=2,padx=50,pady=20)
+# ramka_mapa.grid(row=2, column=0)
 
 
-#ramka lista użytkowników
-
-label_lista_uzytkownikow=Label(ramka_lista_uzytkownikow,text='Lista obiektów')
+# ramka lista obietkow
+label_lista_uzytkownikow=Label(ramka_lista_uzytkownikow,text="Lista uzytkownikow")
 listbox_lista_uzytkownikow=Listbox(ramka_lista_uzytkownikow,width=30)
-button_pokaz_szczegoly=Button(ramka_lista_uzytkownikow,text='pokaż szczegoly')
-button_edytuj_uzytkownika=Button(ramka_lista_uzytkownikow,text='edytuj')
-button_usun_uzytkownika=Button(ramka_lista_uzytkownikow,text='usuń')
+button_pokaz_szcegoly=Button(ramka_lista_uzytkownikow,text="Pokaz szczegoly")
+button_edytuj_uzytkownika=Button(ramka_lista_uzytkownikow,text="Edytuj")
+button_usun_uzytkownika=Button(ramka_lista_uzytkownikow,text="Usuń")
 
-ramka_lista_uzytkownikow.grid(row=0, column=0)
-listbox_lista_uzytkownikow.grid(row=1, column=0,columnspan=3)
-button_pokaz_szczegoly.grid(row=2, column=0)
+label_lista_uzytkownikow.grid(row=0, column=0)
+listbox_lista_uzytkownikow.grid(row=1, column=0, columnspan=3)
+button_pokaz_szcegoly.grid(row=2, column=0)
 button_edytuj_uzytkownika.grid(row=2, column=1)
 button_usun_uzytkownika.grid(row=2, column=2)
 
 # ramka formularz
-
-label_formularz=Label(ramka_formularz,text='formularz edycji i dodawania')
-label_imie=Label(ramka_formularz,text='imie')
-label_nazwisko=Label(ramka_formularz,text='nazwisko')
-label_posty=Label(ramka_formularz,text='posty')
-label_miejsowosc=Label(ramka_formularz,text='miejsowosc')
+label_formularz=Label(ramka_formularz,text="Formularz edycji i dodawania")
+label_imie=Label(ramka_formularz,text="Imię")
+label_nazwisko=Label(ramka_formularz,text="Nazwisko")
+label_postow=Label(ramka_formularz,text="Postów")
+label_miejscowosc=Label(ramka_formularz,text="Miejscowość")
 entry_imie=Entry(ramka_formularz)
 entry_nazwisko=Entry(ramka_formularz)
-entry_posty=Entry(ramka_formularz)
+entry_postow=Entry(ramka_formularz)
 entry_miejscowosc=Entry(ramka_formularz)
+button_dodaj_uzytkownika=Button(ramka_formularz,text="Dodaj")
 
-
-
-
-
-
-label_formularz.grid(row=0, column=0,columnspan=2)
+label_formularz.grid(row=0, column=0, columnspan=2)
 label_imie.grid(row=1, column=0,sticky=W)
 label_nazwisko.grid(row=2, column=0,sticky=W)
-label_posty.grid(row=3, column=0,sticky=W)
-label_miejsowosc.grid(row=4, column=0,sticky=W)
-button_usun_uzytkownika.grid(row=5, column=0,sticky=W)
+label_postow.grid(row=3, column=0,sticky=W)
+label_miejscowosc.grid(row=4, column=0,sticky=W)
 
 entry_imie.grid(row=1, column=1)
 entry_nazwisko.grid(row=2, column=1)
-entry_posty.grid(row=3, column=1)
+entry_postow.grid(row=3, column=1)
 entry_miejscowosc.grid(row=4, column=1)
+button_dodaj_uzytkownika.grid(row=5, column=0, columnspan=2)
+
+# ramka_pokaz_szczegoly
+label_opis_uzytkownika=Label(ramka_pokaz_szczegoly,text="Szczegóły użytkownika")
+label_imie_szczegoly=Label(ramka_pokaz_szczegoly,text="Imię")
+label_imie_szczegoly_wartosc=Label(ramka_pokaz_szczegoly,text="...",width=10)
+label_nazwisko_szczegoly=Label(ramka_pokaz_szczegoly,text="Nazwisko")
+label_nazwisko_szczegoly_wartosc=Label(ramka_pokaz_szczegoly,text="...",width=10)
+label_postow_szczegoly=Label(ramka_pokaz_szczegoly,text="Postów")
+label_postow_szczegoly_wartosc=Label(ramka_pokaz_szczegoly,text="...",width=10)
+label_miejscowosc_szczegoly=Label(ramka_pokaz_szczegoly,text="Miejscowość")
+label_miejscowosc_szczegoly_wartosc=Label(ramka_pokaz_szczegoly,text="...",width=10)
+
+label_opis_uzytkownika.grid(row=0, column=0)
+label_imie_szczegoly.grid(row=1, column=0)
+label_imie_szczegoly_wartosc.grid(row=1, column=1,)
+label_nazwisko_szczegoly.grid(row=1, column=2)
+label_nazwisko_szczegoly_wartosc.grid(row=1, column=3)
+label_postow_szczegoly.grid(row=1, column=4)
+label_postow_szczegoly_wartosc.grid(row=1, column=5)
+label_miejscowosc_szczegoly.grid(row=1, column=6)
+label_miejscowosc_szczegoly_wartosc.grid(row=1, column=7)
 
 
-
-
-#ramka_pokaz_szczegoly
-label_opis_uzytkownika=Label(ramka_mapa,text='szczególy uzytkownika')
-label_imie_szczegoly=Label()
-
-
+label_opis_uzytkownika.grid(row=0, column=0)
 root.mainloop()
